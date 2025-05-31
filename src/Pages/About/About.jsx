df3f8285365a44c4522150cd41c3899e0da64a68
NicolasR3008 <nicolas.rojas_cabal@uao.edu.co>
2025-05-31 12:51:21 -0500

    about

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,25 +1,40 @@
 import React from "react";
-import './About.css'
+import './About.css';
 
 import Imga from "../../assets/Imagensota.png";
+import PrettyBoy from "../../assets/pretty boy.jpg"; 
 
 export const About = () => {
     const teamMembers = [
         {
-            name: "Santiago Gomez",
-            role: "Ingeniero Informatico 6to Semestre",
+            name: "Nicolas Rojas Cabal",
+            role: "Ingeniero Informático 6to semestre",
+            description: "en mi prime dormía más de 3 horas",
+            image: PrettyBoy
+        },
+        {
+            name: "Santiago Gomez Molano",
+            role: "Ingeniero Informático 6to semestre",
             description: "Santiago > Messi (prime)",
-            image: Imga,
+            image: Imga
         },
     ];
 
     return (
         <div className="about-container">
-            <h1>About Us</h1>
+            <h1>About Me</h1>
             <div className="team-members">
                 {teamMembers.map((member, index) => (
-                    <div className="team-member" key={index}>
-                        <img src={member.image} alt={member.name} className="team-member-image" />
+                    <div 
+                        className="team-member" 
+                        key={index} 
+                        style={{ backgroundColor: index % 2 === 0 ? '#86c5a8' : '#7ed957' }}
+                    >
+                        <img 
+                            src={member.image} 
+                            alt={`Foto de ${member.name}`} 
+                            className="team-member-image" 
+                        />
                         <h2>{member.name}</h2>
                         <h3>{member.role}</h3>
                         <p>{member.description}</p>
